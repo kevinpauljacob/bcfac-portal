ea00e9407f3d7e1f631d4e93f436cd2dfb213ad1
Swastik <sr6015@srmist.edu.in>
2023-09-22 14:43:35 +0530

    accordian, table and account page added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-
+import Card from "@/components/dashboard/Card"
+import Accordian from "@/components/dashboard/Accordian";
+import Table from "@/components/dashboard/Table";
 
 export default function Home() {
   return (
     <section>
-
+      <Table />
     </section>
   )
 }
