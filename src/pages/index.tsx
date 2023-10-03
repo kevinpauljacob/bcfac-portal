173f0a2aab6c9cdb7362843f07c06f9117ed5025
Swastik <sr6015@srmist.edu.in>
2023-10-03 12:32:58 +0530

    redirect admin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Card from "@/components/dashboard/Card"
 import Accordian from "@/components/landing/Accordian";
 import Table from "@/components/landing/Table";
+import MainCard from "@/components/landing/MainCard";
+// import { CardData, cardData } from "@/components/landing/CardData";
 
 export default function Home() {
   return (
     <section>
-      <Table />
+      {/* <Table /> */}
       <Accordian/>
     </section>
   )
